b035e6d80c835435bf6b8b62cbff0cddba1be4ec
Abhay Pandey <abhaymp09@gmail.com>
2025-09-11 15:27:58 +0530

    removed dust

--- Test.java
+++
@@ -1,8 +0,0 @@
-import java.io.*;
-class TestMain{
-   public static void main(String args[]) throws IOException{
-       File f = new File("java1");
-       String path =  f.getAbsolutePath();
-       System.out.println(path);
-   }
-}
